2580a62f6660ffb9e707ae280a4c48388218dd65
Chuwi12 <srpc9682@protonmail.com>
2025-11-13 09:41:20 +0100

    Rename class Image.kt

--- app/src/main/java/com/example/p1_artspace_victorperez_sergiorodriguez_pabloalonso/Image.kt
+++ app/src/main/java/com/example/p1_artspace_victorperez_sergiorodriguez_pabloalonso/Image.kt
@@ -2,6 +2,6 @@ package com.example.p1_artspace_victorperez_sergiorodriguez_pabloalonso
 
 import androidx.annotation.DrawableRes
 
-class ImageU(val title: String, val artist: String, @DrawableRes val image: Int) {
+class Image(val title: String, val artist: String, @DrawableRes val image: Int) {
 
 }
