f4ad81005f262dd918f5f6c6fd964643ef3542c5
Chuwi12 <srpc9682@protonmail.com>
2025-11-13 09:40:13 +0100

    Add ImageU.kt class

---
+++ app/src/main/java/com/example/p1_artspace_victorperez_sergiorodriguez_pabloalonso/ImageU.kt
@@ -0,0 +1,7 @@
+package com.example.p1_artspace_victorperez_sergiorodriguez_pabloalonso
+
+import androidx.annotation.DrawableRes
+
+class ImageU(val title: String, val artist: String, @DrawableRes val image: Int) {
+
+}
