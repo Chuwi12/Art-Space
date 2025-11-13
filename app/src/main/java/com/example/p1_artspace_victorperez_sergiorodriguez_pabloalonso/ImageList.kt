ccdd626307e6b2b88535fe5be1f776d053cd321d
Chuwi12 <srpc9682@protonmail.com>
2025-11-13 09:45:11 +0100

    Rename references

--- app/src/main/java/com/example/p1_artspace_victorperez_sergiorodriguez_pabloalonso/ImageList.kt
+++ app/src/main/java/com/example/p1_artspace_victorperez_sergiorodriguez_pabloalonso/ImageList.kt
@@ -1,11 +1,11 @@
 package com.example.p1_artspace_victorperez_sergiorodriguez_pabloalonso
 
-class ImageList(val images: List<Image>) {
+class ImageList(val images: List<Art>) {
     fun count(): Int {
         return images.size;
     }
 
-    fun getImage(index: Int): Image? {
+    fun getImage(index: Int): Art? {
         return images[index];
     }
 }
